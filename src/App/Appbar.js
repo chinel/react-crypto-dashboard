8a38424bcd57c114ae2f2e58fe021616d2f9a9c0
nelo <nelokwaz@gmail.com>
2021-06-03 06:40:15 +0100

    added the base structure of the navbar using grid

--- src/App/Appbar.js
+++ src/App/Appbar.js
@@ -3,10 +3,17 @@ import styled from "styled-components";
 
 const Bar = styled.div`
   display: grid;
+  grid-template-columns: 180px 100px 100px;
 `;
 
 const Appbar = () => {
-  return <Bar>Appbar</Bar>;
+  return (
+    <Bar>
+      <div>CryptoDash</div>
+      <div>Dashboard</div>
+      <div>Settings</div>
+    </Bar>
+  );
 };
 
 export default Appbar;
