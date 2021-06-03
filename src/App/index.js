74ee9ce0aba91a2aa2fe31dabf1a48dbca2eb299
nelo <nelokwaz@gmail.com>
2021-06-03 06:37:22 +0100

    added the app bar to the app component

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Appbar from "./Appbar";
 import AppLayout from "./AppLayout";
 import WelcomeMessage from "./WelcomeMessage";
 
 function App() {
   return (
     <AppLayout>
+      <Appbar />
       <WelcomeMessage />
     </AppLayout>
   );
