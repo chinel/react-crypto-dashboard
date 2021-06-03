de4cb588f3ceb0a6225aad89282b42399c88e4d1
nelo <nelokwaz@gmail.com>
2021-06-03 06:34:36 +0100

    added a skeletal structure of the app bar

---
+++ src/Appbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Bar = styled.div`
+  display: grid;
+`;
+
+const Appbar = () => {
+  return <Bar>Appbar</Bar>;
+};
+
+export default Appbar;
