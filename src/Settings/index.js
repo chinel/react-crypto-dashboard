4ac6892b105355ee474d9fc797fecd8cfbf2feae
nelo <nelokwaz@gmail.com>
2021-06-16 05:59:54 +0100

    added the confirm button component to the settings page

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import WelcomeMessage from "./WelcomeMessage";
+import ConfirmButton from "./ConfirmButton";
 
 export default function Settings() {
-  return <WelcomeMessage />;
+  return (
+    <div>
+      <WelcomeMessage />
+      <ConfirmButton />
+    </div>
+  );
 }
