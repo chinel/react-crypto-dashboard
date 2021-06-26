3c8d893edd05c4c2f1360fb996d538d377197f66
nelo <nelokwaz@gmail.com>
2021-06-26 06:22:57 +0100

    wrapped the nav items with the context consumer

--- src/App/Appbar.js
+++ src/App/Appbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled, { css } from "styled-components";
+import { AppContext } from "./AppProvider";
 
 const Logo = styled.div`
   font-size: 1.5em;
@@ -25,7 +26,13 @@ function toProperCase(lower) {
 }
 
 function ControlButton({ name, active }) {
-  return <ControlButtonElem active={active}>{name}</ControlButtonElem>;
+  return (
+    <AppContext.Consumer>
+      {({ page }) => (
+        <ControlButtonElem active={page === active}>{name}</ControlButtonElem>
+      )}
+    </AppContext.Consumer>
+  );
 }
 
 const Appbar = () => {
