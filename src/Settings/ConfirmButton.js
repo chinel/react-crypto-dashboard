19f226ea86d45690badef8a979e4390aeb5274b3
nelo <nelokwaz@gmail.com>
2021-06-30 05:44:51 +0100

    added the structure of the confirm button component

--- src/Settings/ConfirmButton.js
+++ src/Settings/ConfirmButton.js
@@ -1,2 +1,18 @@
 import React from "react";
 import styled from "styled-components";
+import { AppContext } from "../App/AppProvider";
+
+const ConfirmButtonStyed = styled.div`
+  margin: 20px;
+  color: green;
+`;
+
+export default function ConfirmButton() {
+  return (
+    <AppContext.Consumer>
+      {({ confirmFavorites }) => (
+        <ConfirmButtonStyed>Confirm Favorites</ConfirmButtonStyed>
+      )}
+    </AppContext.Consumer>
+  );
+}
