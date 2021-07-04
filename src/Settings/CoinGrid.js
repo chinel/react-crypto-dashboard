2df43e41de69ad873fd44fcc3c41ac79949872b9
nelo <nelokwaz@gmail.com>
2021-07-04 11:48:09 +0100

    added grid template column to the coin grid

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -4,13 +4,18 @@ import { AppContext } from "../App/AppProvider";
 
 export const CoinGridStyled = styled.div`
   display: grid;
+  grid-template-columns: repeat(5, 1fr);
 `;
 
 const CoinGrid = () => {
   return (
     <AppContext.Consumer>
       {({ coinList }) => (
-        <CoinGridStyled>{Object.keys(coinList).length}</CoinGridStyled>
+        <CoinGridStyled>
+          {Object.keys(coinList).map((coinKey) => (
+            <div>{coinKey}</div>
+          ))}
+        </CoinGridStyled>
       )}
     </AppContext.Consumer>
   );
