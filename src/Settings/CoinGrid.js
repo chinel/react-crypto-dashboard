983a41886583ad63e2b5fdb4f52f65b48b204a64
nelo <nelokwaz@gmail.com>
2021-07-04 11:14:04 +0100

    added the base structure of the coin grid component

---
+++ src/Settings/CoinGrid.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled, { css } from "styled-components";
+import { AppContext } from "../App/AppProvider";
+
+export const CoinGridStyled = styled.div`
+  display: grid;
+`;
+
+const CoinGrid = () => {
+  return (
+    <AppContext.Consumer>
+      {({ coinList }) => (
+        <CoinGridStyled>{Object.keys(coinList).length}</CoinGridStyled>
+      )}
+    </AppContext.Consumer>
+  );
+};
