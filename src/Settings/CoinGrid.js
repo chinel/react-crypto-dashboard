31633be80001bf80c7741fb14c34953027ff7ba1
nelo <nelokwaz@gmail.com>
2021-07-06 13:18:00 +0100

    made some corrections

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled, { css } from "styled-components";
 import { AppContext } from "../App/AppProvider";
-import Tile from "../Shared/Tile";
+import { Tile } from "../Shared/Tile";
 
 export const CoinGridStyled = styled.div`
   display: grid;
