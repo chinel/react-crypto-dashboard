e04949e01a3f794d7b46df225b7863de32ead473
nelo <nelokwaz@gmail.com>
2021-07-06 13:14:24 +0100

    added the base structure for the tile component

---
+++ src/Shared/Tile.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import {
+  subtleBoxShadow,
+  lightBlueBackground,
+  greenBoxShadow,
+  redBoxShadow,
+} from "./Styles";
+
+export const Tile = styled.div`
+  ${subtleBoxShadow}
+  ${lightBlueBackground}
+  padding: 10px;
+`;
