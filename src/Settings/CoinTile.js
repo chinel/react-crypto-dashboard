16110867178bd1d181ccc4200827233399963369
nelo <nelokwaz@gmail.com>
2021-07-11 15:05:29 +0100

    added the coin image to the coin tile component

--- src/Settings/CoinTile.js
+++ src/Settings/CoinTile.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { AppContext } from "../App/AppProvider";
+import CoinImage from "../Shared/CoinImage";
 import { SelectableTile } from "../Shared/Tile";
 import CoinHeaderGrid from "./CoinHeaderGrid";
 
@@ -12,6 +13,7 @@ const CoinTile = ({ coinKey }) => {
         return (
           <TileClass>
             <CoinHeaderGrid name={coin.CoinName} symbol={coin.Symbol} />
+            <CoinImage coin={coin} />
           </TileClass>
         );
       }}
