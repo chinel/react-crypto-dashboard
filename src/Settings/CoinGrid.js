6b05b2e4827b8f3786c98af00a32b5b3555be070
nelo <nelokwaz@gmail.com>
2021-07-11 15:06:29 +0100

    added margin to the coin grid component

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -8,6 +8,7 @@ export const CoinGridStyled = styled.div`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
   grid-gap: 15px;
+  margin-top: 40px;
 `;
 
 const getCoinsToDisplay = (coinList) => {
