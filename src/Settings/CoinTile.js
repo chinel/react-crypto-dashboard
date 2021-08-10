f62b25c1e27391106244724c4a99e7b60b215e55
nelo <nelokwaz@gmail.com>
2021-08-10 07:01:11 +0100

    conditionally rendered the coin tile styled component

--- src/Settings/CoinTile.js
+++ src/Settings/CoinTile.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { AppContext } from "../App/AppProvider";
 import CoinImage from "../Shared/CoinImage";
-import { SelectableTile } from "../Shared/Tile";
+import { SelectableTile, DeletetableTile, DisabledTile } from "../Shared/Tile";
 import CoinHeaderGrid from "./CoinHeaderGrid";
 
 const CoinTile = ({ coinKey, topSection }) => {
@@ -9,10 +9,17 @@ const CoinTile = ({ coinKey, topSection }) => {
     <AppContext.Consumer>
       {({ coinList }) => {
         let coin = coinList[coinKey];
-        const TileClass = SelectableTile;
+        let TileClass = SelectableTile;
+        if (topSection) {
+          TileClass = DeletetableTile;
+        }
         return (
           <TileClass>
-            <CoinHeaderGrid name={coin.CoinName} symbol={coin.Symbol} />
+            <CoinHeaderGrid
+              topSection={topSection}
+              name={coin.CoinName}
+              symbol={coin.Symbol}
+            />
             <CoinImage coin={coin} />
           </TileClass>
         );
