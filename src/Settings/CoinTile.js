c41d04b8afa064ac5ff53e9bd3f529f3f20de187
nelo <nelokwaz@gmail.com>
2021-08-13 06:58:42 +0100

    created a on click handler function to add or remove coins

--- src/Settings/CoinTile.js
+++ src/Settings/CoinTile.js
@@ -4,17 +4,29 @@ import CoinImage from "../Shared/CoinImage";
 import { SelectableTile, DeletetableTile, DisabledTile } from "../Shared/Tile";
 import CoinHeaderGrid from "./CoinHeaderGrid";
 
+const clickCoinHandler = (topSection, coinKey, addCoin, removeCoin) => {
+  return topSection
+    ? () => {
+        removeCoin(coinKey);
+      }
+    : () => {
+        addCoin(coinKey);
+      };
+};
+
 const CoinTile = ({ coinKey, topSection }) => {
   return (
     <AppContext.Consumer>
-      {({ coinList }) => {
+      {({ coinList, addCoin, removeCoin }) => {
         let coin = coinList[coinKey];
         let TileClass = SelectableTile;
         if (topSection) {
           TileClass = DeletetableTile;
         }
         return (
-          <TileClass>
+          <TileClass
+            onClick={clickCoinHandler(topSection, coinKey, addCoin, removeCoin)}
+          >
             <CoinHeaderGrid
               topSection={topSection}
               name={coin.CoinName}
