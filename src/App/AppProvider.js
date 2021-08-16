268702fe992f0ae0c7342f99bafc3a9b6d2986d0
nelo <nelokwaz@gmail.com>
2021-08-16 07:31:51 +0100

    pulled the user favorites coins from the localstorage and store in initial state

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -64,7 +64,8 @@ export class AppProvider extends React.Component {
     if (!cryptoDashData) {
       return { page: "settings", firstVisit: true };
     }
-    return {};
+    let { favorites } = cryptoDashData;
+    return { favorites };
   }
 
   setPage = (page) => this.setState({ page });
