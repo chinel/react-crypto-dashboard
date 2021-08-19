1aca5e852eb1d88c01927c7b497940c9d18b9b2c
nelo <nelokwaz@gmail.com>
2021-08-19 05:33:25 +0100

    created styled component for the search component

--- src/Settings/Search.js
+++ src/Settings/Search.js
@@ -1,8 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 
+const SearchGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`;
+
 const Search = () => {
-  return <div>Search</div>;
+  return <SearchGrid>Search</SearchGrid>;
 };
 
 export default Search;
