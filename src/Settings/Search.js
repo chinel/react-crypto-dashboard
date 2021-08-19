5eeaf6ed5dee5b0e9a10e2f0b8d66cb0bf96818f
nelo <nelokwaz@gmail.com>
2021-08-19 05:40:02 +0100

    worked on the styling for the search component

--- src/Settings/Search.js
+++ src/Settings/Search.js
@@ -1,13 +1,27 @@
 import React from "react";
 import styled from "styled-components";
+import { backgroundColor2, fontSize2 } from "../Shared/Styles";
 
 const SearchGrid = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 200px 1fr;
+`;
+
+const SearchInput = styled.input`
+  ${backgroundColor2}
+  ${fontSize2}
+  height: 25px;
+  color: #1163c9;
+  place-self: center left; // shorthand for align self center and justify self left
 `;
 
 const Search = () => {
-  return <SearchGrid>Search</SearchGrid>;
+  return (
+    <SearchGrid>
+      <h2>Search all coins</h2>
+      <SearchInput />
+    </SearchGrid>
+  );
 };
 
 export default Search;
