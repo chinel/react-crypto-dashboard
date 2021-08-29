263ac6447bcbef0afc09edce2a32a0694679d363
nelo <nelokwaz@gmail.com>
2021-08-29 09:06:23 +0100

    added a loading indicator to show when prices are being fetch and if user not first time

--- src/Shared/Content.js
+++ src/Shared/Content.js
@@ -4,10 +4,14 @@ import { AppContext } from "../App/AppProvider";
 const Content = (props) => {
   return (
     <AppContext.Consumer>
-      {({ coinList }) => {
+      {({ coinList, prices, firstVisit }) => {
         if (!coinList) {
           return <div>Loading Coins</div>;
         }
+        if (!firstVisit && !prices) {
+          return <div>Loading Prices</div>;
+        }
+
         return <div>{props.children}</div>;
       }}
     </AppContext.Consumer>
