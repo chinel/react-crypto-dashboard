cf03a8c61b971c551ce2c2c5b5567d1d4854a268
nelo <nelokwaz@gmail.com>
2021-08-30 06:15:56 +0100

    created the dashboard page

---
+++ src/Dashboard/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import WelcomeMessage from "./WelcomeMessage";
+import ConfirmButton from "./ConfirmButton";
+import Page from "../Shared/Page";
+import CoinGrid from "./CoinGrid";
+import Search from "./Search";
+
+export default function Settings() {
+  return (
+    <Page name="settings">
+      <WelcomeMessage />
+      <CoinGrid topSection />
+      <ConfirmButton />
+      <Search />
+      <CoinGrid />
+    </Page>
+  );
+}
