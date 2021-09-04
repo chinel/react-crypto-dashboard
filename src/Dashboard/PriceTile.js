ea96eba2bdfb6ea098240e9beb5386cad8074e47
nelo <nelokwaz@gmail.com>
2021-09-04 07:23:09 +0100

    created a price tile div component wrapper for the price tile component

--- src/Dashboard/PriceTile.js
+++ src/Dashboard/PriceTile.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled, { css } from "styled-components";
+import { CoinHeaderGridStyled } from "../Settings/CoinHeaderGrid";
 import { fontSize3 } from "../Shared/Styles";
 import { SelectableTile } from "../Shared/Tile";
 
@@ -10,6 +11,17 @@ const PriceTileStyled = styled(SelectableTile)`
       ${fontSize3}
     `}
 `;
+
+const PriceTileDiv = ({ sym, data }) => {
+  return (
+    <PriceTileStyled>
+      <CoinHeaderGridStyled>
+        <div>{sym}</div>
+        <div>{data.CHANGEPCT24HOUR}</div>
+      </CoinHeaderGridStyled>
+    </PriceTileStyled>
+  );
+};
 
 const PriceTile = ({ price, index }) => {
   let sym = Object.keys(price)[0];
