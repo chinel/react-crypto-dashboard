a6952eb0e47a12dad1a9ab1231415cf9772af31c
nelo <nelokwaz@gmail.com>
2021-09-15 06:42:27 +0100

    added the coin spot light to the dashboard page

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Page from "../Shared/Page";
+import CoinSpotlight from "./CoinSpotlight";
 import PriceGrid from "./PriceGrid";
 
 export default function Dashboard() {
   return (
     <Page name="dashboard">
       <PriceGrid />
+      <CoinSpotlight />
     </Page>
   );
 }
