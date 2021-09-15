2b7e7f912ad2b9d7e0c0185f43c207e86ae0ee70
nelo <nelokwaz@gmail.com>
2021-09-15 06:30:08 +0100

    created a coin spot light component

---
+++ src/Dashboard/CoinSpotlight.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { AppContext } from "../App/AppProvider";
+import CoinImage from "../Shared/CoinImage";
+import { Tile } from "../Shared/Tile";
+
+const CoinSpotlight = () => {
+  return <Tile>Hello Spot</Tile>;
+};
+
+export default CoinSpotlight;
