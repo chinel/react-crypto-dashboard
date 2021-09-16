c69085ef170e405aefd7dfccc54319347b8f91e4
nelo <nelokwaz@gmail.com>
2021-09-16 07:01:50 +0100

    created a styled component for the spotlight title element

--- src/Dashboard/CoinSpotlight.js
+++ src/Dashboard/CoinSpotlight.js
@@ -1,7 +1,12 @@
 import React from "react";
+import styled from "styled-components";
 import { AppContext } from "../App/AppProvider";
 import CoinImage from "../Shared/CoinImage";
 import { Tile } from "../Shared/Tile";
+
+const SpotlightName = styled.h2`
+  text-align: center;
+`;
 
 const CoinSpotlight = () => {
   return (
