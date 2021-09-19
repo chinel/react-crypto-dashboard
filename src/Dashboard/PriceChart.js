77bbc83d6b425abe4dd8c7f533f7e6a36ebceea4
nelo <nelokwaz@gmail.com>
2021-09-19 07:00:25 +0100

    created the base structure for the price chart component

--- src/Dashboard/PriceChart.js
+++ src/Dashboard/PriceChart.js
@@ -3,3 +3,15 @@ import ReactHighcharts from "react-highcharts";
 import { AppContext } from "../App/AppProvider";
 import { Tile } from "../Shared/Tile";
 import highchartsConfig from "./HighchartsConfig";
+
+const PriceChart = () => {
+  return (
+    <AppContext.Consumer>
+      <Tile>
+        <ReactHighcharts config={highchartsConfig()} />
+      </Tile>
+    </AppContext.Consumer>
+  );
+};
+
+export default PriceChart;
